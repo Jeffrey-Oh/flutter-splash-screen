0c6d66d0a0ce06f7dea1625629f31c08a8326188
오태현 <jeffrey@kai-i.com>
2022-06-09 22:28:42 +0900

    StatelessWidget 사용해보기

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,17 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('test'),
-        ),
-      ),
-    ),
+    MaterialApp(home: HomeScreen()),
   );
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Hello World'),
+      ),
+    );
+  }
 }
